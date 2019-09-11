fea0c5fc8912bc0fde9b510a2e521249b616200e
Tyla Devon <tyladevon@gmail.com>
2019-09-11 15:50:56 -0600

    Refactor and add goals_avg_by_hoa helper method
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -20,6 +20,18 @@ module LeagueStatistics
     goal_avg_by_team
   end
 
+  def goal_avg_by_hoa_helper(hoa)
+    games_by_team = @game_teams.group_by(&:team_id)
+    goal_avg_by_team = {}
+    games_by_team.each do |team_id, games_arr|
+      away_games_goals = games_arr.find_all do |game|
+        game.hoa == hoa
+      end.sum(&:goals)
+      goal_avg_by_team[team_id] = (away_games_goals.to_f / games_arr.length).round(2)
+    end
+    goal_avg_by_team
+  end
+
   def best_offense
     goal_avg_by_team ||= goal_avg_helper
     found_team_id = goal_avg_by_team.key(goal_avg_by_team.values.max_by { |value| value})
@@ -32,7 +44,7 @@ module LeagueStatistics
     team_name_helper(found_team_id)
   end
 
-  def best_defense
+  def defense_helper
     home_teams = {}
     @games.each_value do |game|
       if !home_teams.has_key?(game.home_team_id)
@@ -58,116 +70,43 @@ module LeagueStatistics
       total_goals = goals_arr.sum
       goal_avg_by_team[team_id] = (total_goals.to_f / goals_arr.length).round(2)
     end
+    goal_avg_by_team
+  end
 
+  def best_defense
+    goal_avg_by_team ||= defense_helper
     best_defense = goal_avg_by_team.key(goal_avg_by_team.values.min_by { |value| value})
-    @teams.select do |team_id, team|
-      best_defense == team_id
-    end[best_defense].team_name
+    team_name_helper(best_defense)
   end
 
   def worst_defense
-    home_teams = {}
-    @games.each_value do |game|
-      if !home_teams.has_key?(game.home_team_id)
-        home_teams[game.home_team_id] = [game.away_goals]
-      else
-        home_teams[game.home_team_id].push(game.away_goals)
-      end
-    end
-
-    away_teams = {}
-    @games.each_value do |game|
-      if !away_teams.has_key?(game.away_team_id)
-        away_teams[game.away_team_id] = [game.home_goals]
-      else
-        away_teams[game.away_team_id].push(game.home_goals)
-      end
-    end
-
-    teams = home_teams.merge(away_teams) { |team_id, home_team_val, away_team_val| home_team_val + away_team_val}
-
-    goal_avg_by_team = {}
-    teams.each do |team_id, goals_arr|
-      total_goals = goals_arr.sum
-      goal_avg_by_team[team_id] = (total_goals.to_f / goals_arr.length).round(2)
-    end
-
+    goal_avg_by_team ||= defense_helper
     worst_defense = goal_avg_by_team.key(goal_avg_by_team.values.max_by { |value| value})
-    @teams.select do |team_id, team|
-      worst_defense == team_id
-    end[worst_defense].team_name
+    team_name_helper(worst_defense)
   end
 
   def highest_scoring_visitor
-    games_by_team = @game_teams.group_by do |game_team|
-      game_team.team_id
-    end
-    goal_avg_by_team = {}
-    games_by_team.each do |team_id, games_arr|
-      away_games_goals = games_arr.find_all do |game|
-        game.hoa == "away"
-      end.sum { |game| game.goals }
-      goal_avg_by_team[team_id] = (away_games_goals.to_f / games_arr.length).round(2)
-    end
-
+    goal_avg_by_team ||= goal_avg_by_hoa_helper("away")
     highest_team_id = goal_avg_by_team.key(goal_avg_by_team.values.max_by { |value| value})
-    @teams.select do |team_id, team|
-      highest_team_id == team_id
-    end[highest_team_id].team_name
+    team_name_helper(highest_team_id)
   end
 
   def highest_scoring_home_team
-    games_by_team = @game_teams.group_by do |game_team|
-      game_team.team_id
-    end
-    goal_avg_by_team = {}
-    games_by_team.each do |team_id, games_arr|
-      away_games_goals = games_arr.find_all do |game|
-        game.hoa == "home"
-      end.sum { |game| game.goals }
-      goal_avg_by_team[team_id] = (away_games_goals.to_f / games_arr.length).round(2)
-    end
-
+    goal_avg_by_team ||= goal_avg_by_hoa_helper("home")
     highest_team_id = goal_avg_by_team.key(goal_avg_by_team.values.max_by { |value| value})
-    @teams.select do |team_id, team|
-      highest_team_id == team_id
-    end[highest_team_id].team_name
+    team_name_helper(highest_team_id)
   end
 
   def lowest_scoring_visitor
-      games_by_team = @game_teams.group_by do |game_team|
-        game_team.team_id
-      end
-      goal_avg_by_team = {}
-      games_by_team.each do |team_id, games_arr|
-        away_games_goals = games_arr.find_all do |game|
-          game.hoa == "away"
-        end.sum { |game| game.goals }
-        goal_avg_by_team[team_id] = (away_games_goals.to_f / games_arr.length).round(2)
-      end
-
-      highest_team_id = goal_avg_by_team.key(goal_avg_by_team.values.min_by { |value| value})
-      @teams.select do |team_id, team|
-        highest_team_id == team_id
-      end[highest_team_id].team_name
+    goal_avg_by_team ||= goal_avg_by_hoa_helper("away")
+    highest_team_id = goal_avg_by_team.key(goal_avg_by_team.values.min_by { |value| value})
+    team_name_helper(highest_team_id)
   end
 
   def lowest_scoring_home_team
-    games_by_team = @game_teams.group_by do |game_team|
-      game_team.team_id
-    end
-    goal_avg_by_team = {}
-    games_by_team.each do |team_id, games_arr|
-      away_games_goals = games_arr.find_all do |game|
-        game.hoa == "home"
-      end.sum { |game| game.goals }
-      goal_avg_by_team[team_id] = (away_games_goals.to_f / games_arr.length).round(2)
-    end
-
+    goal_avg_by_team ||= goal_avg_by_hoa_helper("home")
     highest_team_id = goal_avg_by_team.key(goal_avg_by_team.values.min_by { |value| value})
-    @teams.select do |team_id, team|
-      highest_team_id == team_id
-    end[highest_team_id].team_name
+    team_name_helper(highest_team_id)
   end
 
   def winningest_team
